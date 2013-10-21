ee5d44ea53da546149bfd5b32f3e6e830c919a4a
Jon Cram <jon@webignition.net>
2013-10-21 16:11:40 +0100

    Test parsing invalid xml finds nothing ... good ...

--- tests/ParsePerformanceTest.php
+++ tests/ParsePerformanceTest.php
@@ -8,11 +8,19 @@ class ParsePerformanceTest extends BaseTest {
         $this->setTestFixturePath(__CLASS__, $this->getName());
     }
     
-    public function testAddChildToNonIndexSitemap() {        
+    public function testParseSomewhatVeryLargeSitemap() {        
         $sitemap = $this->createSitemap();
         $sitemap->setUrl('http://example.com/sitemap.xml');
         $sitemap->setContent($this->getFixture('SitemapsOrgXmlWith8497Urls.xml'));
         
         $this->assertEquals(8497, count($sitemap->getUrls()));
+    }      
+    
+    public function testParseInvalidXmlSitemap() {        
+        $sitemap = $this->createSitemap();
+        $sitemap->setUrl('http://example.com/sitemap.xml');
+        $sitemap->setContent($this->getFixture('SitemapsOrgXmlInvalid.xml'));
+        
+        $this->assertEquals(0, count($sitemap->getUrls()));
     }      
 }
